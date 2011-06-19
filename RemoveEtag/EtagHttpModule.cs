100a8b00745b13abbc2dcdd718f24c4b41d94523
Stephen von Takach <steve@advancedcontrol.com.au>
2011-06-19 18:37:39 +1000

    Initial upload - currently running in production

---
+++ RemoveEtag/EtagHttpModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace RemoveEtag
+{
+	public class EtagHttpModule : IHttpModule
+	{
+		public void Init(HttpApplication context)
+		{
+			context.EndRequest += new EventHandler(context_EndRequest);
+		}
+
+		private void context_EndRequest(object sender, EventArgs e)
+		{
+			var context = HttpContext.Current;
+			context.Response.Headers.Remove("ETag");
+		}
+		public void Dispose() { }
+	}
+}
